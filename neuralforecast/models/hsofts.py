09ceb4dd3e1036a5f15a761a2a861b82df9c6e8c
hljubic <hrvoje.ljubic@gmail.com>
2024-08-09 18:28:14 +0200

    HSOFTS KAN

--- neuralforecast/models/hsofts.py
+++ neuralforecast/models/hsofts.py
@@ -275,6 +275,55 @@ class HSOFTS(BaseMultivariate):
         # Diferenciranje
         diff_x_enc = x_enc[:, :, 1:] - x_enc[:, :, :-1]
 
+        self.alpha = 0.1
+        # EWMA zaglađivanje originalnog niza
+        ewma_x_enc = torch.zeros_like(diff_x_enc)
+        ewma_x_enc[:, :, 0] = diff_x_enc[:, :, 0]  # Početna vrednost
+        for t in range(1, diff_x_enc.shape[2]):
+            ewma_x_enc[:, :, t] = self.alpha * diff_x_enc[:, :, t] + (1 - self.alpha) * ewma_x_enc[:, :, t-1]
+
+        # Inversija niza i EWMA zaglađivanje inverznog niza
+        reversed_diff_x_enc = diff_x_enc[:, :, ::-1]
+        ewma_reversed_x_enc = torch.zeros_like(reversed_diff_x_enc)
+        ewma_reversed_x_enc[:, :, 0] = reversed_diff_x_enc[:, :, 0]  # Početna vrednost
+        for t in range(1, reversed_diff_x_enc.shape[2]):
+            ewma_reversed_x_enc[:, :, t] = self.alpha * reversed_diff_x_enc[:, :, t] + (1 - self.alpha) * ewma_reversed_x_enc[:, :, t-1]
+
+        # Vraćanje inverznog niza u normalan redosled
+        ewma_reversed_x_enc = ewma_reversed_x_enc[:, :, ::-1]
+
+        # Aritmetička sredina dva zaglađena niza
+        smoothed_x_enc = (ewma_x_enc + ewma_reversed_x_enc) / 2
+
+        _, _, N = smoothed_x_enc.shape
+        enc_out = self.enc_embedding(smoothed_x_enc, None)
+        enc_out, attns = self.encoder(enc_out, attn_mask=None)
+        dec_out = self.projection(enc_out).permute(0, 2, 1)
+
+        # Vraćanje diferenciranih vrednosti u izvorne vrednosti
+        dec_out = torch.cat([x_enc[:, :, :1], dec_out], dim=2)
+        dec_out = torch.cumsum(dec_out, dim=2)
+
+        # De-normalizacija iz Non-stationary Transformer-a
+        if self.use_norm:
+            dec_out = dec_out * (stdev[:, 0, :].unsqueeze(1).repeat(1, self.h, 1))
+            dec_out = dec_out + (means[:, 0, :].unsqueeze(1).repeat(1, self.h, 1))
+
+        return dec_out
+
+    def forecast2(self, x_enc):
+        # Normalizacija iz Non-stationary Transformer-a
+        if self.use_norm:
+            means = x_enc.mean(1, keepdim=True).detach()
+            x_enc = x_enc - means
+            stdev = torch.sqrt(
+                torch.var(x_enc, dim=1, keepdim=True, unbiased=False) + 1e-5
+            )
+            x_enc /= stdev
+
+        # Diferenciranje
+        diff_x_enc = x_enc[:, :, 1:] - x_enc[:, :, :-1]
+
         _, _, N = diff_x_enc.shape
         enc_out = self.enc_embedding(diff_x_enc, None)
         enc_out, attns = self.encoder(enc_out, attn_mask=None)
