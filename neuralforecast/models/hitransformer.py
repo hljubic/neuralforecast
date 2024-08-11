7bee361c04d467b5abd6f6b3da627443177413e5
hljubic <hrvoje.ljubic@gmail.com>
2024-08-11 19:02:25 +0200

    Promjene

--- neuralforecast/models/hitransformer.py
+++ neuralforecast/models/hitransformer.py
@@ -370,17 +370,17 @@ class HiTransformer(BaseMultivariate):
                         FullAttention(
                             False, self.factor, attention_dropout=self.dropout
                         ),
-                        self.hidden_size * 2,  # Adjust for concatenated embeddings
+                        self.hidden_size * 1,  # Adjust for concatenated embeddings
                         self.n_heads,
                     ),
-                    self.hidden_size * 2,  # Adjust for concatenated embeddings
+                    self.hidden_size * 1,  # Adjust for concatenated embeddings
                     self.d_ff,
                     dropout=self.dropout,
                     activation=F.gelu,
                 )
                 for l in range(self.e_layers)
             ],
-            norm_layer=torch.nn.LayerNorm(self.hidden_size * 2),
+            norm_layer=torch.nn.LayerNorm(self.hidden_size * 1),
         )
 
         # Adjust the projector layer to match the new hidden size
@@ -400,13 +400,13 @@ class HiTransformer(BaseMultivariate):
 
         # Embedding
         # DataEmbedding_inverted
-        enc_out_data = self.enc_embedding(x_enc, None)
+        #enc_out_data = self.enc_embedding(x_enc, None)
 
         # DiffEmbedding
-        enc_out_diff = self.diff_embedding(x_enc)
+        enc_out = self.diff_embedding(x_enc)
 
         # Concatenate the two embeddings along the feature dimension
-        enc_out = torch.cat([enc_out_data, enc_out_diff], dim=2)  # Concatenate along the feature dimension
+        #enc_out = torch.cat([enc_out_data, enc_out_diff], dim=2)  # Concatenate along the feature dimension
 
         # Encode the concatenated embeddings
         enc_out, attns = self.encoder(enc_out, attn_mask=None)
