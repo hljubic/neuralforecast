304f5edd778537ba95d46554cd9a203390425908
hljubic <hrvoje.ljubic@gmail.com>
2024-08-13 16:57:25 +0200

    Promjene

--- neuralforecast/models/hsofts.py
+++ neuralforecast/models/hsofts.py
@@ -240,7 +240,7 @@ class HSOFTS(BaseMultivariate):
         dec_out3 = self.projector3(segment3).permute(0, 2, 1)
 
         # Concatenate the three outputs
-        dec_out = torch.cat([dec_out1, dec_out2, dec_out3], dim=2)
+        dec_out = torch.cat([dec_out1, dec_out2, dec_out3], dim=1)
 
         #dec_out = self.final(dec_out)
 
