2569636e09693eef331d55b1790650170d6d6240
hljubic <hrvoje.ljubic@gmail.com>
2024-08-23 12:08:22 +0200

    Promjene

--- neuralforecast/models/hsofts.py
+++ neuralforecast/models/hsofts.py
@@ -319,6 +319,8 @@ class HSOFTS(BaseMultivariate):
         # Residuals (differences from smoothed data)
         residual_x_enc = x_enc - smoothed_x_enc
 
+        residual_x_enc = self.normalize_frequencies(residual_x_enc, 0.5)
+
         # Encoding with separate layers
         enc_smooth_out = self.encoder_smooth(self.enc_embedding(smoothed_x_enc))
         enc_residual_out = self.encoder_residual(self.enc_embedding(residual_x_enc))
