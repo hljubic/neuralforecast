656f37c75438252cc780cb8a9fedb0167e455387
hljubic <hrvoje.ljubic@gmail.com>
2024-08-23 12:12:32 +0200

    Promjene

--- neuralforecast/models/hsofts.py
+++ neuralforecast/models/hsofts.py
@@ -314,7 +314,7 @@ class HSOFTS(BaseMultivariate):
             x_enc = (x_enc - means) / stdev
 
         # Smoothed data (e.g., Gaussian filter)
-        smoothed_x_enc = self.gaussian_filter(x_enc, kernel_size=3, sigma=1.75)
+        smoothed_x_enc = self.gaussian_filter(x_enc, kernel_size=3, sigma=4.75)
 
         # Residuals (differences from smoothed data)
         residual_x_enc = x_enc - smoothed_x_enc
